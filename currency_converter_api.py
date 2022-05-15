4a6d3cac49a3055190e2b4be7eb53152aed25a65
SRCinVA <scitarella@hotmail.com>
2022-05-14 20:42:35 -0400

    More progress

--- currency_converter_api.py
+++ currency_converter_api.py
@@ -1,7 +1,7 @@
 from requests import get
 from pprint import PrettyPrinter
 
-BASE_URL = "https://freecurrcov.com/"
+BASE_URL = "https://free.currconv.com/"
 API_KEY = "89fa56e292ed77a89e10"
 
 # need to send some query parameters if we want to get some specific data
@@ -9,4 +9,22 @@ API_KEY = "89fa56e292ed77a89e10"
 
 printer = PrettyPrinter()  # remember that PrettyPrinter is a formating tool for JSON.
 
-endpoint = f"api/v7/currencies?apiKey={API_KEY}" # remember that '?' starts the query parameter.
+def get_currencies():
+    endpoint = f"api/v7/currencies?apiKey={API_KEY}" # remember that '?' starts the query parameter.
+    url = BASE_URL + endpoint
+    data = get(url).json()['results'] # 'results' will be a dictionary ...
+
+    data = list(data.items()) # ... then we cast it as a list.
+    data.sort() # defaults to alphabetical order
+
+    return data
+
+def print_currencies(currencies):
+    for name, currency in currencies: # we have to give two items because it's a list of tuples
+        name = currency["currencyName"]
+        _id = currency["id"]
+        symbol = currency.get("currencySymbol", " ") # get() will give you the value or the default (an empty string)s
+        print(f"{_id} - {name} - {symbol}")
+
+data = get_currencies()
+print_currencies(data)
